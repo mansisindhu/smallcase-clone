b584d232670dbd3e3a33811d172f949360fdddf3
Mansi Sindhu <mansisindhu2909@gmail.com>
2021-08-26 13:30:10 +0530

    modal payment

--- smallcaseDetail/smallcaseDetail.js
+++ smallcaseDetail/smallcaseDetail.js
@@ -133,6 +133,10 @@ function renderData() {
     buttonEl.className = "payment-section-btn";
     buttonEl.textContent = "Invest Now";
 
+    buttonEl.addEventListener("click", function() {
+        renderModal();
+    })
+
     paymentSectionDivEl.appendChild(buttonEl);
 
     const paymentSectioneResponsiveDivEl = document.createElement("div");
@@ -169,3 +173,116 @@ function renderData() {
 }
 
 renderData();
+
+function closeModal() {
+    const modalOverlay = document.querySelector('.modal-overlay');
+    modalOverlay.remove();
+}
+
+
+const renderModal = () => {
+
+    const rootElement = document.querySelector(".root");
+
+    const modalOverlay = document.createElement('div');
+    modalOverlay.setAttribute('class', 'modal-overlay');
+
+    const modalContainer = document.createElement('div');
+    modalContainer.setAttribute('class', 'modal-container');
+
+    const topRowEl = document.createElement("div");
+    topRowEl.className = "top-row";
+
+    const imageEl = document.createElement("img");
+    imageEl.src = product.image;
+
+    const nameEl = document.createElement("p");
+    nameEl.textContent = `Investing in ${product.name}`;
+
+    const closeButton = document.createElement('p');
+    closeButton.className  = "close-modal";
+    closeButton.textContent = 'X';
+
+    closeButton.addEventListener('click', closeModal)
+
+    topRowEl.appendChild(imageEl);
+    topRowEl.appendChild(nameEl);
+    topRowEl.appendChild(closeButton);
+
+
+    const midRowEl = document.createElement("div");
+    midRowEl.className = "mid-row";
+
+    const headingEl = document.createElement("h3");
+    headingEl.textContent = "Confirm investment amount";
+
+    const minimumAmountEl = document.createElement("p");
+    minimumAmountEl.textContent = `Min. Investment amount : ₹ ${product.minAmount}`
+
+    midRowEl.appendChild(headingEl);
+    midRowEl.appendChild(minimumAmountEl);
+
+    const amountBoxEl = document.createElement("div");
+    amountBoxEl.className = "amount-box";
+    amountBoxEl.textContent = `₹ ${product.minAmount}`;
+
+    const incrementBtnsEl = document.createElement("div");
+    incrementBtnsEl.className = "increment-btns";
+
+    const oneKButton = document.createElement("button");
+    oneKButton.textContent = "+1000";
+
+    const fiveKButton = document.createElement("button");
+    fiveKButton.textContent = "+5000";
+
+    const tenKButton = document.createElement("button");
+    tenKButton.textContent = "+10000";
+
+
+    incrementBtnsEl.appendChild(oneKButton);
+    incrementBtnsEl.appendChild(fiveKButton);
+    incrementBtnsEl.appendChild(tenKButton);
+
+
+    const paymentBtnElement = document.createElement("button");
+    paymentBtnElement.textContent = "Invest Now";
+    paymentBtnElement.className = "payment-section-btn-modal";
+
+    paymentBtnElement.addEventListener("click", setToLocalStorage(product));
+
+
+    modalContainer.appendChild(topRowEl);
+    modalContainer.appendChild(midRowEl);
+    modalContainer.appendChild(amountBoxEl);
+    modalContainer.appendChild(incrementBtnsEl);
+    modalContainer.appendChild(paymentBtnElement);
+
+
+    modalOverlay.appendChild(modalContainer);
+
+    rootElement.appendChild(modalOverlay);
+}
+
+
+
+function setToLocalStorage(product) {
+    return function() {
+        closeModal();
+        const orderData = JSON.parse(window.localStorage.getItem("orders")) || [];
+        orderData.push(product);
+        const orderDataJson = JSON.stringify(orderData);
+        window.localStorage.setItem("orders", orderDataJson);
+    }
+}
+
+
+
+[
+    {
+        name: "",
+        date: "",
+        status: "",
+        batch: "",
+        image: "",
+    }
+]
