d9fbb94ad9703fdc4fbbf8be65841aa76acfc0cc
Mansi Sindhu <mansisindhu2909@gmail.com>
2021-09-28 22:33:45 +0530

    user model

---
+++ src/models/user.model.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    email: {type: String, required: true},
+    mobile: {type:Number, required:true},
+    password: {type: String, required:true},
+    orders: [{type: mongoose.Schema.Types.ObjectId, ref: "smallcase_data" ,required: false}]
+})
+
+module.exports = mongoose.model("user_data", userSchema);
