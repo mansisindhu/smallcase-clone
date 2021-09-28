c30ea81b9da02e5407e9a57ff0e5b0420e36a2e3
Mansi Sindhu <mansisindhu2909@gmail.com>
2021-09-28 22:33:22 +0530

    smallcase model

---
+++ src/models/smallcase.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const smallcaseSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    shortDescription: { type: String, required: true },
+    img: { type: String, required: true },
+    cagYear: { type: Number, required: true },
+    cagRate: { type: Number, required: true },
+    minAmount: { type: Number, required: true },
+    volatility: { type: String, required: true },
+    factSheet: { type: String, required: true },
+    ques_title: { type: String, required: true },
+    discription: [{ type: String, required: true }]
+})
+
+module.exports = mongoose.model("smallcase", smallcaseSchema);
