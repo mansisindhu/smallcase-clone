287a9e86101384773840f4bcdbd2e215439315cf
Mansi Sindhu <mansisindhu2909@gmail.com>
2021-09-29 13:55:39 +0530

    smallcase detail controller

---
+++ src/controllers/smallcaseDetail.controller.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const SmallCase = require("../models/smallcase.model.js");
+
+const router = express.Router();
+
+router.get("/:id", async function (req, res) {
+    const smallcaseData = await SmallCase.find({"id": req.params.id}).lean().exec();
+
+    res.render("smallcaseDetail", {
+        data : smallcaseData
+    });
+});
+
+module.exports = router;
