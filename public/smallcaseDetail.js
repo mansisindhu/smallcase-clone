3c352137aa8cee298d0074a0c871711c7ec4a61d
Mansi Sindhu <mansisindhu2909@gmail.com>
2021-09-30 10:34:25 +0530

    added js file to render modal

---
+++ public/smallcaseDetail.js
@@ -0,0 +1,105 @@
+function closeModal() {
+    const modalOverlay = document.querySelector('.modal-overlay');
+    modalOverlay.remove();
+}
+
+const renderModal = (product) => {
+    
+    let totalPrice = product.minAmount;
+    const rootElement = document.querySelector(".root");
+
+    const modalOverlay = document.createElement('div');
+    modalOverlay.setAttribute('class', 'modal-overlay');
+
+    const modalContainer = document.createElement('div');
+    modalContainer.setAttribute('class', 'modal-container');
+
+    const topRowEl = document.createElement("div");
+    topRowEl.className = "top-row";
+
+    const imageEl = document.createElement("img");
+    imageEl.src = product.img;
+
+    const nameEl = document.createElement("p");
+    nameEl.textContent = `Investing in ${product.title}`;
+
+    const closeButton = document.createElement('p');
+    closeButton.className = "close-modal";
+    closeButton.textContent = 'X';
+
+    closeButton.addEventListener('click', closeModal)
+
+    topRowEl.appendChild(imageEl);
+    topRowEl.appendChild(nameEl);
+    topRowEl.appendChild(closeButton);
+
+
+    const midRowEl = document.createElement("div");
+    midRowEl.className = "mid-row";
+
+    const headingEl = document.createElement("h3");
+    headingEl.textContent = "Confirm investment amount";
+
+    const minimumAmountEl = document.createElement("p");
+    minimumAmountEl.className = "minimum-amt";
+    minimumAmountEl.textContent = `Min. Investment amount : ₹ ${product.minAmount}`
+
+    midRowEl.appendChild(headingEl);
+    midRowEl.appendChild(minimumAmountEl);
+
+    const amountBoxEl = document.createElement("div");
+    amountBoxEl.className = "amount-box";
+    amountBoxEl.textContent = `₹ ${product.minAmount}`;
+
+    const incrementBtnsEl = document.createElement("div");
+    incrementBtnsEl.className = "increment-btns";
+
+    const oneKButton = document.createElement("button");
+    oneKButton.textContent = "+1000";
+
+    oneKButton.addEventListener("click", incrementAmount(1000))
+
+    const fiveKButton = document.createElement("button");
+    fiveKButton.textContent = "+5000";
+
+    fiveKButton.addEventListener("click", incrementAmount(5000))
+
+
+    const tenKButton = document.createElement("button");
+    tenKButton.textContent = "+10000";
+
+    tenKButton.addEventListener("click", incrementAmount(10000))
+
+
+    incrementBtnsEl.appendChild(oneKButton);
+    incrementBtnsEl.appendChild(fiveKButton);
+    incrementBtnsEl.appendChild(tenKButton);
+
+    const paymentBtnElement = document.createElement("button");
+    paymentBtnElement.textContent = "Invest Now";
+    paymentBtnElement.className = "payment-section-btn-modal";
+
+    modalContainer.appendChild(topRowEl);
+    modalContainer.appendChild(midRowEl);
+    modalContainer.appendChild(amountBoxEl);
+    modalContainer.appendChild(incrementBtnsEl);
+    modalContainer.appendChild(paymentBtnElement);
+
+    modalOverlay.appendChild(modalContainer);
+
+    rootElement.appendChild(modalOverlay);
+
+    
+function incrementAmount(price) {
+    return function () {
+        const amountBoxEl = document.querySelector(".amount-box");
+        const minimumAmountElement = document.querySelector(".minimum-amt");
+
+        totalPrice += price;
+
+        amountBoxEl.textContent = `₹ ${totalPrice}`;
+        minimumAmountElement.textContent = `Min. Investment amount : ₹ ${totalPrice}`
+    }
+}
+
+}
