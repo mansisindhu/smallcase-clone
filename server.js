ffcac70a925d01587ac0be0765fd55f8f07b3d37
Mansi Sindhu <mansisindhu2909@gmail.com>
2021-09-30 23:58:20 +0530

    api made for filters

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const connect = require("./src/config/dbconfig");
 const smallCaseController = require("./src/controllers/landingPage.controller");
 const allSmallcasesController = require("./src/controllers/allSmallcases.controller");
 const smallcaseDetailController = require("./src/controllers/smallcaseDetail.controller");
+const Smallcases = require("./src/models/smallcase.model");
 
 app.use("/", smallCaseController);
 app.use("/discover/all", allSmallcasesController);
@@ -41,8 +42,31 @@ app.get("/orders", async (req, res) => {
   res.render("orders")
 })
 
+
+// filters api
+app.get('/api/data', async (req, res) => {
+  let tags;
+  if (req.query.volatility) {
+    tags = req.query.volatility.split(",");
+  }
+  const underMinAmount = req.query.under || 100000000;
+  tags = tags || ["Low", "High", "Med."];
+  let data = await Smallcases.find({ $and: [{ "minAmount": { $lt: underMinAmount } }, { "volatility": { $in: tags } }] });
+
+  let filteredData = data;
+  if (req.query.sortBy === "minAmount") {
+    filteredData.sort((a, b) => a.minAmount - b.minAmount);
+  } else if (req.query.sortBy === "returns") {
+    filteredData.sort((a, b) => b.cagRate - a.cagRate);
+  } else if (req.query.sortBy === "popularity") {
+    filteredData = data;
+  }
+
+  res.send(filteredData);
+})
+
 // server
 app.listen(2930, async () => {
-    await connect();
-    console.log("listening to port 2930");
+  await connect();
+  console.log("listening to port 2930");
 })
