82e7e367eac289aecc76727490cc3b7eae45534e
Mansi Sindhu <mansisindhu2909@gmail.com>
2021-10-01 10:23:15 +0530

    user schema edited

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     email: {type: String, required: true},
-    mobile: {type:Number, required:true},
     password: {type: String, required:true},
     orders: [{type: mongoose.Schema.Types.ObjectId, ref: "smallcase_data" ,required: false}]
 })
