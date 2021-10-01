dd77b895eb324c28505446e2569daf7379d3a322
Mansi Sindhu <mansisindhu2909@gmail.com>
2021-10-01 22:56:10 +0530

    header styles added

--- public/common.js
+++ public/common.js
@@ -11,10 +11,16 @@ loginBtnElement.addEventListener("click", async () => {
 
 const orderBtnElement = document.querySelector(".order-btn");
 
-orderBtnElement.addEventListener("click", async () => {
+orderBtnElement.addEventListener("click", () => {
     if (loginBtnElement.textContent === "Login") {
         orderBtnElement.href = "/login";
     } else if (loginBtnElement.textContent === "Logout") {
         orderBtnElement.href = "/orders";
     }
 })
+
+if (window.location.pathname === "/orders") {
+    document.querySelector(".order-btn").className = "active-header";
+} else if (window.location.pathname === "/discover/explore" || window.location.pathname === "/discover/all") {
+    document.getElementById("discover").className = "active-header";
+}
