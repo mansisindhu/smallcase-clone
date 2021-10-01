b11fa897ba7403c826352b364357aa88c33b9938
Mansi Sindhu <mansisindhu2909@gmail.com>
2021-10-01 10:22:48 +0530

    login signupt api added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,18 +3,34 @@ const User = require("../models/user.model");
 
 const router = express.Router();
 
-router.get("", async (req, res) => {
-    const users = await User.find().lean().exec();
-    res.render('url', {
-        users
-    });
+router.post("/signup", async (req, res) => {
+    const regx = /^([a-zA-Z0-9\.-]+)\@([a-zA-Z0-9\-]+)\.([a-z]{2,20})$/;
+    if (!regx.test(req.body.email)) {
+        res.status(400).send({message: "Invaild email address", error: true});
+        return;
+    }
+    const usersData = await User.find({email: req.body.email}).lean().exec();
+    if (usersData.length) {
+        res.status(409).send({message: "User already exists", error: true});
+        return;
+    } else {
+        const user = await User.create(req.body);
+        res.status(201).send(user);
+    }
 })
 
-//get smallData with User
-router.get("/:id/smallcase_data", async (req, res) => {
-    const user = await User.find({ post: req.params.id }).lean().exec();
-    const smallCase = await smallCase.findById(req.params.id).lean().exec();
-    return res.status(201).send({ user, smallCase });
+router.post("/login", async (req, res) => {
+    const userData = await User.find({email: req.body.email}).lean().exec();
+    if (userData.length) {
+        if (userData[0].password === req.body.password) {
+            res.cookie("userId", userData[0]._id.toString(), {httpOnly: true});
+            res.status(200).send({});
+        } else {
+            res.status(403).send({message: "Wrong password", error: true})
+        }
+    } else {
+        res.status(403).send({message: "Login failed", error: true});
+    }
 })
 
 module.exports = router;
