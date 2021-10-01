cf5c4d4210c33ed7727c881a86bf6cb6d732265c
Mansi Sindhu <mansisindhu2909@gmail.com>
2021-10-01 22:58:59 +0530

    date field added in user schema

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    email: {type: String, required: true},
-    password: {type: String, required:true},
-    orders: [{type: mongoose.Schema.Types.ObjectId, ref: "smallcase_data" ,required: false}]
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    orders: [
+        {
+            smallcase_id: { type: mongoose.Schema.Types.ObjectId, ref: "smallcase_data", required: false },
+            date: { type: String, required: false }
+        }
+    ]
 })
 
 module.exports = mongoose.model("user_data", userSchema);
