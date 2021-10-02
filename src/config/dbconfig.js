a784843c383e8a6f56324a09b9d2392a0e305b35
Mansi Sindhu <mansisindhu2909@gmail.com>
2021-10-02 19:53:17 +0530

    removed extra comments

--- src/config/dbconfig.js
+++ src/config/dbconfig.js
@@ -3,5 +3,4 @@ const mongoose = require("mongoose");
 module.exports = () => {
     // link to connect to online database
     return mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.nab9t.mongodb.net/smallcase?retryWrites=true&w=majority`);
-    // const connectionString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@coding-blog-t0xf0.mongodb.net/<dbname>`
 }
