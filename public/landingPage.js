6744485bad4ef6573f35b1e10d3f792fdb103d33
Mansi Sindhu <mansisindhu2909@gmail.com>
2021-10-03 14:08:40 +0530

    accordian logic added

--- public/landingPage.js
+++ public/landingPage.js
@@ -24,3 +24,19 @@ carouselContainerElement.addEventListener("transitionend", function () {
         interval = setInterval(onInterval, 4000);
     }
 })
+
+
+// Accordian 
+var acc = document.getElementsByClassName("first");
+for (var i = 0; i < acc.length; i++) {
+    acc[i].addEventListener('click', function () {
+        this.classList.toggle("active");
+        var demo = this.nextElementSibling;
+        if (demo.style.display === "block") {
+            demo.style.display = "none";
+        } else {
+            demo.style.display = "block";
+        }
+    });
+}
+
